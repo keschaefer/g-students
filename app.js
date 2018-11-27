74c9f206af55dbaae80b77a9391bc4985aaddd63
keschaefer <keschaefer@gmail.com>
2018-11-27 11:25:52 -0700

    added seed data to table

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const knex = require('knex')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
