6089e1bd642456dcd8a62ab0b553d2676df38c4f
keschaefer <keschaefer@gmail.com>
2018-11-27 10:02:18 -0700

    added additional dependencies, created database

---
+++ app.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
+
+app.get('/', (req, res, next) => {
+   res.send("That route worked!")
+})
+
+
+
+
+
+app.use((req, res, next) => {
+   res.status(404).json({ error: { message: 'data not found' }})
+ })
+
+app.use((err, req, res, next) => {
+   const status = err.status || 500
+   res.status(status).json({ error: err})
+ })
+
+const listener = () => `Listening on port ${port}!`
+app.listen(port, listener)
+
+
+module.exports = app
