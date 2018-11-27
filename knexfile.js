8a1451e51a757ccbc2f79e16a5acb57dc14dc86a
keschaefer <keschaefer@gmail.com>
2018-11-27 10:21:30 -0700

    added gitignore file and configured knexfile

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgresql://localhost/G-STUDENTS-102-DATABASE'
+  },
+
+  production: {
+    client: 'pg',
+    connection: 'process.env.DATABASE_URL',
+  }
+
+};
+
